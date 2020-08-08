41cf9f44f21c4df7a625357ecd15063778a48fdb
Niels <git@nierot.com>
2020-08-08 12:11:01 +0200

    get gamecode from gamehub

---
+++ utils/gamecode.js
@@ -0,0 +1,12 @@
+const fetch = require('node-fetch');
+const settings = require('../settings.json');
+
+module.exports = {
+    getGamecode: async port => {
+        return new Promise((resolve, reject) => {
+            fetch(settings.gamehub_uri)
+                .then(gamecode => resolve(gamecode))
+                .catc(err => reject(err));
+        })
+    }
+}
