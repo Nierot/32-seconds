bd290e770aae0b87b2d3eee045f5198807e84d01
Nierot <56843328+Nierot@users.noreply.github.com>
2020-08-10 18:51:24 +0200

    Logging

--- app.js
+++ app.js
@@ -9,19 +9,23 @@ const webRoutes = require('./routes/web');
 const restfulRoutes = require('./routes/rest');
 const { parentPort } = require('worker_threads');
 const socketHandler = require('./handlers/socket');
+const { Log } = require('nielog');
 
 let users = {};
 
 app.get('/words', body_parser.json(), (req, res) => restfulRoutes.words(req, res, db));
 app.get('/*', webRoutes.game);
 
-io.on('connection', socket => socketHandler(socket, users, io));
+io.on('connection', socket => socketHandler(socket, users, io, Log));
 
 app.use(cors);
 app.set('view engine', 'ejs');
 app.use(body_parser);
 
 const listener = http.listen(settings.development ? 8079 : 0, () => {
-    if (settings.gamehub.active) parentPort.postMessage(listener.address().port)
+    if (settings.gamehub.active) parentPort.postMessage(listener.address().port);
+
+    settings.development ? Log.setLevel(1) : Log.setLevel(2);
+
     console.log(`Listening on http://localhost:${listener.address().port} `);
 });
