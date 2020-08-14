7276643f3ecfd775eec0f26cfd585b338832476e
Nierot <56843328+Nierot@users.noreply.github.com>
2020-08-14 18:27:18 +0200

    rest

--- routes/rest.js
+++ routes/rest.js
@@ -1,3 +1,7 @@
+const identification = require('../utils/identification');
+const { verbose } = require('sqlite3');
+const Game = require('../data/Game');
+
 module.exports = {
     words: async (req, res, db) => {
         console.dir(req.body);
@@ -21,5 +25,25 @@ module.exports = {
         let x = await db.getAllListsWithWords('niels')
         console.log(x);
         return res.send(await (await db).getWords(options));
-    }
+    },
+
+    connect: async (req, res, users) => {
+        let id = identification.generateUserID();
+        let spec = users.length > 2;
+        let json = { id: id, spectator: spec }
+        res.json(json);
+        users.push(json);
+        verbose(users)
+    },
+
+    init: async (req, res, users, gamecode, game) => {
+        game = new Game(gamecode);
+        res.json({ gamecode: gc });
+        verbose('Started a new game with gamecode: ' + gc);
+    },
+
+    identify: async (req, res, users) => {
+        console.log(req.body);
+        res.status(200).send('ok');
+    },
 }
